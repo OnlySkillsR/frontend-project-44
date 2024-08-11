de09cf67dd3ceca2072d3af75b0a6e02d36348d3
piro <piromanter@yandex.ru>
2024-08-11 14:14:29 +0300

    update brain-games

--- src/cli.js
+++ src/cli.js
@@ -2,9 +2,11 @@
 import readlineSync from 'readline-sync';
 
 // eslint-disable-next-line import/prefer-default-export
-export const askName = () => {
+const askName = () => {
   const name = readlineSync.question('May I have your name? ');
 
   // eslint-disable-next-line no-console
   console.log(`Hello, ${name}!`);
 };
+
+export default askName;

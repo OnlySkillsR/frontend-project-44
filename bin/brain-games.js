bf61c90f3bfdb7a5f2185685a6ed0ab5065db5d6
piro <piromanter@yandex.ru>
2024-08-11 13:57:20 +0300

    update after linter

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 
-// eslint-disable-next-line import/extensions
 import { askName } from '../src/cli.js';
 
-// eslint-disable-next-line no-console
 console.log('Welcome to the Brain Games!');
 
 askName();
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-// eslint-disable-next-line import/extensions
 import { gameStarter } from '../src/games/gcd.js';
 
 gameStarter();

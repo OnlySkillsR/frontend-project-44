2a46ff5bcfcfcad21cdd6b7d38b6e47c2fb20c28
piro <piromanter@yandex.ru>
2024-08-11 14:15:15 +0300

    update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-import { askName } from '../src/cli.js';
+// eslint-disable-next-line import/extensions
+import askName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 
